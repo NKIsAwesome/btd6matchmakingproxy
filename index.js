8740d251a15f2337bf32e8019ec801de45a9edc0
NKIA <nkwasawesome@gmail.com>
2020-07-11 11:47:45 -0400

    Fix stupid header and make it work.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const axios = require('axios');
-//const request = require('request-promise');
+//const axios = require('axios');
+const request = require('request-promise');
 const bodyParser = require('body-parser');
 const app = express();
 const app2 = express();
 const PORT = 8085;
 const PORT2 = 8086;
+
+
 
 app.use(bodyParser.json());
 app2.use(bodyParser.json());
@@ -26,17 +28,20 @@ app2.use((req,res,next)=>{
 
 const handleRequest = async (req,res,url)=>{
 	console.log(req.body);
-	const response = await axios.post(url,req.body,{
+	/*const response = await axios.post(url,req.body,{
 		headers:req.headers,
-	}).catch(e=>console.error(e));
-	/*const response = await request({
+	}).catch(e=>console.error(e));*/
+	req.headers.host = 'api.ninjakiwi.com';
+	const response = await request({
+		//servername :'api.ninjakiwi.com',
+		//hostname :'api.ninjakiwi.com',
 		uri:url,
 		method:'POST',
 		body:JSON.stringify(req.body),
 		headers:req.headers
-	}).catch(e=>console.error(e));*/
-	//res.send(response);
-	res.send('lol');
+	}).catch(e=>console.error(e));
+	console.log(response);
+	res.send(response);
 }
 
 
